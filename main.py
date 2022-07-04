19e7653be6b60617641b9d5f82d90237d3434c21
Varmilo <90532063+VarmiloVA@users.noreply.github.com>
2022-07-04 08:26:23 +0200

    Statement deleted from main.py
    Because it has been added to README.md

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-#   Reto #25
-#   PIEDRA, PAPEL, TIJERA
-#   Fecha publicación enunciado: 20/06/22
-#   Fecha publicación resolución: 27/06/22
-#   Dificultad: MEDIA
- 
-#   Enunciado: Crea un programa que calcule quien gana más partidas al piedra, papel, tijera.
-#   - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
-#   - La función recibe un listado que contiene pares, representando cada jugada.
-#   - El par puede contener combinaciones de "R" (piedra), "P" (papel) o "S" (tijera).
-#   - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
-#
-#   Reto propuesto y resulto (en Kotlin) por Brais Moure Aka: MoureDev
-
 from random import sample
 from time import sleep
 
